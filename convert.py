df5907adc3cffd1cb279e61825e77a2f5b591a72
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-03-03 00:58:38 +0100

    Add converter script

---
+++ convert.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""Export Hetionet in several BEL formats."""
+
+import os
+
+import click
+from pybel import get_hetionet, to_bel_script_gz, to_graphdati_gz, to_nodelink_gz
+
+
+@click.command()
+@click.option('--directory', default=os.getcwd(), type=click.Path(dir_okay=True, file_okay=False))
+def main(directory: str):
+    """Export Hetionet in several BEL formats."""
+    click.echo('Getting hetionet')
+    graph = get_hetionet()
+
+    click.echo('Exporting BEL Script')
+    script_gz_path = os.path.join(directory, 'hetionet-v1.0.bel.gz')
+    to_bel_script_gz(graph, script_gz_path)
+
+    click.echo('Exporting Nodelink')
+    nodelink_gz_path = os.path.join(directory, 'hetionet-v1.0.bel.nodelink.json.gz')
+    to_nodelink_gz(graph, nodelink_gz_path)
+
+    click.echo('Exporting GraphDati')
+    graphdati_gz_path = os.path.join(directory, 'hetionet-v1.0.bel.graphdati.json.gz')
+    to_graphdati_gz(graph, graphdati_gz_path)
+
+
+if __name__ == '__main__':
+    main()
